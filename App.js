209dff6f891583e5cae22c33927f9a6f4f70564a
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2018-07-16 23:33:53 +0200

    Create LoginForm component. Install react-native-elements library.

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import LoginForm from "./src/components/LoginForm";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <LoginForm/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+};
---
+++ src/components/LoginForm.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {View} from "react-native";
+import {Button} from "react-native-elements";
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <View>
+        <Button title="Login"/>
+      </View>
+    )
+  }
+}
+
+export default LoginForm;
